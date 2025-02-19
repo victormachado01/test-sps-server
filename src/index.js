7b3c771d56fbac75ae89be53b7059a538552d6ab
victorcamardo01 <vrcmachado@outlook.com>
2025-02-18 21:26:31 -0300

    Fix: Importando dotenv no arquivo main para ter acesso ao .env - Mudando a porta do servidor para 8080

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log("Server is running on http://localhost:3000");
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
